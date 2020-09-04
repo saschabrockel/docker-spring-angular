7b740d5ebe4140a3ab75838f73c843e72a22ee79
CptDayDreamer <gsydaydreamer@gmail.com>
2020-09-05 00:29:47 +0200

    Initial commit

---
+++ src/main/java/com/brockel/dockerspringangular/DockerSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.brockel.dockerspringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brockel/dockerspringangular/DockerSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brockel.dockerspringangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
